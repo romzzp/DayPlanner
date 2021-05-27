1d3e0f5583ab75e169728a3159ca818c94574fdd
romzz.git <romzz.patel@gmail.com>
2021-05-28 04:02:53 +0530

    added clear button to html, tweaked css and added function to js

--- script.js
+++ script.js
@@ -11,3 +11,46 @@ $(document).ready(function () {
     update();
     setInterval(update, 1000);
 });
+
+// $(document).ready(function () {
+var time = moment();
+
+function dayScheduler() {
+    thisHour = time.hours();
+    $(".timeSlot").each(function () {
+        var workHour = parseInt($(this).attr("id"));
+        if (workHour > thisHour) {
+            $(this).addClass("future")
+        }
+        else if (workHour === thisHour) {
+            $(this).addClass("present");
+        }
+        else {
+            $(this).addClass("past");
+        }
+    })
+}
+dayScheduler();
+
+function saveWork() {
+    $(".timeSlot").each(function () {
+        var id = $(this).attr("id");
+        var work = localStorage.getItem(id);
+        if (work !== null) {
+            $(this).children(".work").val(work);
+        }
+    });
+}
+saveWork();
+
+var saveBtn = $(".saveBtn");
+saveBtn.on("click", function () {
+    var time = $(this).parent().attr("id");
+    var work = $(this).siblings(".work").val();
+    localStorage.setItem(time, work);
+});
+
+var clearBtn = $(".clearBtn");
+clearBtn.on("click", function () {
+    localStorage.clear();
+});
